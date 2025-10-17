e4099e7efc0965b5564d2fde66ee6087024f9949
Onamusi Babajide <50235388+NamsyJay@users.noreply.github.com>
2025-10-17 10:06:44 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.lancash.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.lancash.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.DefaultVPC_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.lancash_subnet[*].id
 }
 
